9ab03e92dca4050b8ed9cb7ebe1a4342e4239d92
Mottttton <mottttton.webdev@gmail.com>
2024-02-22 12:10:42 +0900

    modify class_name & redirect

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to tasks_path, notice: t('.updated')
+      redirect_to task_path(@task.id), notice: t('.updated')
     else
       render :edit
     end

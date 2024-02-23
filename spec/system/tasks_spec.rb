6d95aa9ab3102f14cfe066dec0695c8430a20b46
Mottttton <mottttton.webdev@gmail.com>
2024-02-24 07:02:37 +0900

    modify FactoryBot and System Spec

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,28 +2,48 @@ require 'rails_helper'
 
 RSpec.describe 'タスク管理機能', type: :system do
   describe '登録機能' do
-
     context 'タスクを登録した場合' do
       it '登録したタスクが表示される' do
-        task = FactoryBot.create(:task)
-        visit tasks_path
-        expect(page).to have_content '書類作成'
-        second_task = FactoryBot.create(:second_task)
-        visit tasks_path
-        expect(page).to have_content 'メール送信'
+        visit new_task_path
+        fill_in('task_title', with: 'new task')
+        fill_in('task_content', with: 'new task')
+        click_button('登録する')
+        task_list = all('body tr')
+        expect(page).to have_table
+        expect(task_list[1].text).to have_text('new task')
       end
     end
   end
 
   describe '一覧表示機能' do
+    let!(:first_task) { FactoryBot.create(:first_task) }
+    let!(:second_task) { FactoryBot.create(:second_task) }
+    let!(:third_task) { FactoryBot.create(:third_task) }
+
+    before do
+      visit tasks_path
+    end
+
     context '一覧画面に遷移した場合' do
-      it '登録済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task)
-        second_task = FactoryBot.create(:second_task)
-        visit tasks_path
+      it '登録済みのタスク一覧が作成日時の降順で表示される' do
+        task_list = all('body tr')
         expect(page).to have_table
-        expect(page).to have_content(task.title)
-        expect(page).to have_content(second_task.title)
+        expect(task_list[1].text).to have_text('third_task')
+        expect(task_list.last.text).to have_text('first_task')
+      end
+    end
+    context '新たにタスクを作成した場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('body tr')
+        expect(task_list[1].text).not_to have_text("latest")
+
+        visit new_task_path
+        fill_in('task_title', with: 'latest')
+        fill_in('task_content', with: 'latest task')
+        click_button('登録する')
+        task_list = all('body tr')
+        expect(page).to have_table
+        expect(task_list[1].text).to have_text("latest")
       end
     end
   end
@@ -31,7 +51,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it 'そのタスクの内容が表示される' do
-        task = FactoryBot.create(:task)
+        task = FactoryBot.create(:first_task)
         visit task_path(task.id)
         expect(page).to have_text(task.title)
         expect(page).to have_text(task.content)

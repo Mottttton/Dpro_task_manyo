86c15afa1ea0cbac3c1811e7ac31290122adf1b6
Mottttton <mottttton.webdev@gmail.com>
2024-02-26 16:28:58 +0900

    modify task table: add columns and modify models & views

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,12 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  validates :deadline_on, presence: true
+  validates :priority, presence: true
+  enum priority: [:低, :中, :高]
+  validates :status, presence: true
+  enum status: [:未着手, :着手中, :完了]
+
+  scope :title_search, ->(part) {where("title like ?", "%#(part)%")}
+  scope :status_search, ->(status) {where(status: status)}
 end

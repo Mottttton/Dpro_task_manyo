6c05c71b4110fa55880267af2781a2e20b74c0c2
Mottttton <mottttton.webdev@gmail.com>
2024-03-05 11:55:46 +0900

    modify routes.rb: remove 'except [:index]'

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   resources :tasks
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, except: [:index]
+  resources :users
   namespace :admin do
     resources :users
   end
